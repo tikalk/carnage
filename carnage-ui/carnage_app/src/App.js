ddeb7d5853c5be993df8e65fa2d4eeebaf52f264
roybirger <birger.roy@gmail.com>
2018-02-20 15:47:20 +0200

    login

--- carnage-ui/carnage_app/src/App.js
+++ carnage-ui/carnage_app/src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-// import Login from './login/login.component';
+import Login from './login/login.component';
 import Question from './question/question.component';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 
-  index = 0;
-
+  index = 1;
+  loggedIn = false;
   static URL = 'https://807npuj887.execute-api.us-west-1.amazonaws.com/dev/question/'
 
-  question = {
-    question: "what is your name?",
-    answers: [
-      'Roy',
-      'Itay',
-      'Keren',
-      'Pinhas'
-    ]
-  }
+  // question = {
+  //   question: "what is your name?",
+  //   answers: [
+  //     'Roy',
+  //     'Itay',
+  //     'Keren',
+  //     'Pinhas'
+  //   ]
+  // }
 
   render() {
+    let self = this;
     return (
       <div className="App">
-        <Question question={this.question} onSubmit={this.onQuestionSubmitted}></Question>
+      {
+        !this.loggedIn && <Login onLogin={() => {self.logInUser()}}></Login>
+      }      
+      {
+        this.loggedIn && this.index > 1 && <Question question={this.question} onSubmit={this.onQuestionSubmitted}></Question>
+      }
+        
       </div>
     );
   }
@@ -50,6 +57,11 @@ class App extends Component {
       })(this));
     
     
+  }
+
+  logInUser() {
+    this.loggedIn = true;
+    this.forceUpdate();
   }
 }
 
